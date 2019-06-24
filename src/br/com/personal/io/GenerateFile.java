9cc1a5788ec96abb80f47ceaf7866882c9fe2d6b
douglasgp <douglasgaldino22@hotmail.com>
2019-06-23 22:58:44 -0300

    Captura e armazenamento de registro de abastecimento

--- src/br/com/personal/io/GenerateFile.java
+++ src/br/com/personal/io/GenerateFile.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 import br.com.personal.io.ManagerIo;
 
 public class GenerateFile {
+	public static int CONT = 0;
 	public static int TOTAL_CAMPOS = 8;
+	public static String tipoStrCombustivel;
+	public static int tipoCombustivel = 0;
+	private static String typeFuel[] = new String[tipoCombustivel];
+
 	public static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
@@ -13,14 +18,49 @@ public class GenerateFile {
 		Fuel fuel = new Fuel();
 		ManagerIo opManager = new ManagerIo();
 		
-		// Solicita data de abastecimento
+
+		// Solicita DATA de abastecimento
 		System.out.println("Data: ");
 		String data = scan.nextLine();
-		// Encapsulamento de POO
-		fuel.setDateSupplies(data);
 		
-		// Soli
+		// Solicita CÓDIGO DE CUPOM FISCAL
+		System.out.println("CCF: ");
+		int ccf = scan.nextInt();
+		
+		// Solicita mais um tipo de combustível, se houver
+		// Solicita TIPO de combustível
+		int valor = 0;
+		do {
+			System.out.println("Tipo combustível: ");
+			System.out.println("1. Gasolina\n"
+					+ "2. Gasolina Aditivada\n"
+					+ "3. Etanol\n"
+					+ "4. Etanol Aditivado\n"
+					+ "5. Diesel\n"
+					+ "6. Outros\n"
+					+ "0. Sair");
+			CONT= scan.nextInt();
+			
+			// Invoca método de verificação para o tipo de combustível retornando uma String
+			// tipoStrCombustivel = opManager.verificaTipoCombustivel(tipoCombustivel);
+			typeFuel[CONT] = opManager.verificaTipoCombustivel(CONT);
+			
+			// tipoCombustivel = scan.nextInt();
+			CONT++;
+		} while (valor != 0);
+		ManagerIo.verificaTipoCombustivel(tipoCombustivel);
+		
+		// Encapsulamento de dados
+		fuel.setDateSupplies(data);
+		fuel.setCcf(ccf);
+		fuel.setTypeFuel(tipoStrCombustivel);
+		
 		System.out.println("Data informada: " + fuel.getDateSupplies());
-
+		System.out.println("CCF: " + fuel.getCcf());
+		for (int i = 0; i < CONT; i++) {
+			System.out.println("Tipo combustível: " + tipoStrCombustivel);
+			
+		}
+		
 	}
 }
