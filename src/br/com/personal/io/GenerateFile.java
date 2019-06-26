960e09c69d3aa552c06e1bc17d81fed50c36161e
douglasgp <douglasgaldino22@hotmail.com>
2019-06-25 23:09:10 -0300

    Normalização de comentários e invocação de métodos
    Cálculo de combustível baseado em métodos Java, utilizando retorno de
    parâmetros

--- src/br/com/personal/io/GenerateFile.java
+++ src/br/com/personal/io/GenerateFile.java
@@ -10,10 +10,12 @@ public class GenerateFile {
 	public static int TOTAL_CAMPOS = 8;
 	public static String tipoStrCombustivel;
 	public static int tipoCombustivel = 0;
+	public static double VALOR_TOTAL = 0;
 	
 	public static int vetorTypeFuel[] = new int[QTD_COMBUSTIVEL];
 	public static String typeFuel[] = new String[QTD_COMBUSTIVEL];
 	public static double valorTypeFuel[] = new double[QTD_COMBUSTIVEL];
+	public static double QTD_LITRO[] = new double[QTD_COMBUSTIVEL];
 
 	public static Scanner scan = new Scanner(System.in);
 
@@ -23,29 +25,37 @@ public class GenerateFile {
 		ManagerIo opManager = new ManagerIo();
 		
 
-		// Solicita DATA de abastecimento
+		// 1.1. Solicita DATA de abastecimento
 		System.out.println("Data: ");
 		String data = scan.nextLine();
 		
-		// Solicita CÓDIGO DE CUPOM FISCAL
+		//1.2. Solicita CÓDIGO DE CUPOM FISCAL
 		System.out.println("CCF: ");
 		int ccf = scan.nextInt();
 		
-		// Estrutura para adicionar um ou mais tipo/valor de combustível
+		//2.1. Estrutura para adicionar um ou mais tipo/valor de combustível
 		int opcao = 0;
 		do {
-			// Menu de opção para inserir informações de combustível
+			//2.2. Menu de opção para inserir informações de combustível
 			System.out.println("Adicionar Combustível?");
 			System.out.println("1. Sim\n"
 					+ "2. Não\n");
 			opcao = scan.nextInt();
+			// 2.3. Estrutura para enviar e receber parâmetros/informações do combustível
 			if(opcao == 1) {
-				// Armazena numa posição do vetor, o código retornado pelo método
+				//2.4. Armazena numa posição do vetor, o código retornado pelo método
 				vetorTypeFuel[CONT] = ManagerIo.escolheCombustivel(opcao);
-				// Armazena numa posição do vetor nome do combustível retornado pelo método
+				//2.5 Armazena numa posição do vetor nome do combustível retornado pelo método
 				// Envio de parâmetro INT e retorno de parâmetro STRING
 				typeFuel[CONT] = ManagerIo.verificaTipoCombustivel(vetorTypeFuel[CONT]);
-				// contador soma +1 para ser utilizado pelo vetor caso usuário adicione mais combustível nesta seção
+								
+				valorTypeFuel[CONT] = ManagerIo.registraPrecoCombustivel();
+				
+				QTD_LITRO[CONT] = ManagerIo.registraLitroCombustivel();
+				
+				VALOR_TOTAL = ManagerIo.calculaValorTotal(valorTypeFuel[CONT], QTD_LITRO[CONT]);
+				
+				//2.6 contador soma +1 para ser utilizado pelo vetor caso usuário adicione mais combustível nesta seção
 				CONT++;
 			}
 		} while (opcao != 2);
@@ -55,19 +65,21 @@ public class GenerateFile {
 			System.out.println("Combustível: " + typeFuel[i]);
 		}*/
 		
-		// ManagerIo.verificaTipoCombustivel(tipoCombustivel);
-		
-		// Encapsulamento de dados
+		//3.1. Encapsulamento de dados
 		fuel.setDateSupplies(data);
 		fuel.setCcf(ccf);
 		fuel.setTypeFuel(tipoStrCombustivel);
 		
+		// 4.1. Exibe dados informados e gerados ao decorrer da sessão
 		System.out.println("Data informada: " + fuel.getDateSupplies());
 		System.out.println("CCF: " + fuel.getCcf());
+		System.out.println("Valor total: R$ "+VALOR_TOTAL);
 		for (int i = 0; i < CONT; i++) {
+			System.out.println(" = = = = = " + typeFuel[i] + " = = = = = ");
 			System.out.println("Código: " + vetorTypeFuel[i]);
-			System.out.println("Combustível: " + typeFuel[i]);
-			// System.out.println("Valor: " + valorTypeFuel[i]);
+			System.out.println("Preço/litro: R$ " + valorTypeFuel[i]);
+			System.out.println("Qtd/litro: " + QTD_LITRO[i]);
+			System.out.println();
 		}
 		
 	}
