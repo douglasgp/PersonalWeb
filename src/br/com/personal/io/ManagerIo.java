52fd3f4b8be2b06f3269cdf78a857c2823c3aed6
douglasgp <douglasgaldino22@hotmail.com>
2019-06-25 23:05:25 -0300

    Método solicita preço e qtd e calcula valor total (Java)

--- src/br/com/personal/io/ManagerIo.java
+++ src/br/com/personal/io/ManagerIo.java
@@ -4,40 +4,55 @@ import java.util.Scanner;
 
 public class ManagerIo {
 	public static Scanner scan = new Scanner(System.in);
+	public static String fuelStr = null;
 	public static int VALOR = 6;
 	public static int CONT = 0;
 	public static String[] typeFuel = new String[VALOR];
 	public static double valueFuel[]= new double[VALOR];
 	
 	public static String verificaTipoCombustivel(int valor) {
-		String fuelStr = null;
 		if(valor == 1) {
 			fuelStr = "Gasolina";
-			CONT++;
+			System.out.println("Você escolheu: " + fuelStr);
 		}else if(valor == 2) {
 			fuelStr = "Gasolina Aditivada";
-			CONT++;
+			System.out.println("Você escolheu: " + fuelStr);
 		}else if(valor == 3) {
 			fuelStr = "Etanol Comum";
-			CONT++;
+			System.out.println("Você escolheu: " + fuelStr);
 		}else if(valor == 4) {
 			fuelStr = "Etanol Aditivado";
-			CONT++;
+			System.out.println("Você escolheu: " + fuelStr);
 		}else if(valor == 5) {
 			fuelStr = "Diesel";
-			CONT++;
+			System.out.println("Você escolheu: " + fuelStr);
 		}else if(valor == 6) {
 			fuelStr = "Outros";
-			CONT++;
+			System.out.println("Você escolheu: " + fuelStr);
 		}else {
 			System.out.println("Opção inválida!");
 			System.exit(0);
-		}
+		}		
 		return fuelStr;
 	}
 	
-	public static void registraPrecoCombustivel() {
-		
+	public static double registraPrecoCombustivel() {
+		System.out.println(" = = = = = " + fuelStr + " = = = = = ");
+		System.out.println("Informe VALOR por litro: ");
+		double valorComb = scan.nextDouble();
+		return valorComb;
+	}
+	
+	public static double registraLitroCombustivel() {
+		System.out.println(" = = = = = " + fuelStr + " = = = = = ");
+		System.out.println("Informe QUANTIDADE de litros:");
+		double qtdLitros = scan.nextDouble();
+		return qtdLitros;
+	}
+	
+	public static double calculaValorTotal(double preco, double litro) {
+		double total = preco * litro;
+		return total;
 	}
 	public void adicionaCombustivel() {
 		
