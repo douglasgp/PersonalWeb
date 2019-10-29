9c18c1c37ae6be4c59c9ec3a9a533034685d2f01
linux02@douglasgp.net <douglasgaldino22@hotmail.com>
2019-10-28 21:14:56 -0300

    Estrutura de suporte web

---
+++ src/br/com/personal/modelo/Supply.java
@@ -0,0 +1,23 @@
+package br.com.personal.modelo;
+
+public class Supply {
+	/* Declaração das Variáveis */
+	
+	/* Construtor com parâmetros */
+	
+	/* Método para adicionar elementos */
+	
+	/* Adiciona elemento em determinada posição */
+	
+	/* Verifica a capacidade do vetor e dobra seu tamanho se necessário */
+	
+	/* Busca elemento na posição informada */
+	
+	/* Realiza comparação se dois elementos (strings) são idênticos */
+	
+	/* Método para retornar tamanho */
+	
+	/* Imprime o vetor de elementos */
+	
+
+}
